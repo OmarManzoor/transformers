15a81a1bf9c56cbf80f6a68d849751055781a070
Omar Salman <omar.salman@arbisoft.com>
2024-05-27 19:47:13 +0500

    Update _no_split_modules

--- src/transformers/models/blip/modeling_blip_text.py
+++ src/transformers/models/blip/modeling_blip_text.py
@@ -552,7 +552,7 @@ class BlipTextPreTrainedModel(PreTrainedModel):
 
     config_class = BlipTextConfig
     base_model_prefix = "bert"
-    _no_split_modules = ["BlipTextEmbeddings", "BlipTextAttention"]
+    _no_split_modules = ["BlipTextEmbeddings", "BlipTextAttention", "nn.Embedding"]
 
     def _init_weights(self, module):
         """Initialize the weights"""
